06c35ea08a9d210f223856a5e93910c227ab2a60
nurseld <61293125+nurseld@users.noreply.github.com>
2022-09-21 21:36:53 +0300

    Homework1

---
+++ 06-switchDemo/src/SwitchDemo.java
@@ -0,0 +1,28 @@
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		
+        char grade = 'B';
+
+        switch (grade){
+            case 'A':
+                System.out.println("Excellent!");
+                break;
+            case 'B':
+            case 'C':
+                System.out.println("Good!");
+                break;
+            case 'D':
+                System.out.println("Passed!");
+                break;
+            case 'F':
+                System.out.println("Failure!");
+                break;
+            default:
+                System.out.println("Invalid grade entered.");
+                break;
+        }
+	}
+
+}
