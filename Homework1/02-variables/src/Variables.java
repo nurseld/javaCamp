71627b97a801011f04536b0bad6f42e931081c8d
nurseld <61293125+nurseld@users.noreply.github.com>
2022-09-27 22:47:54 +0300

    Homework1

---
+++ Homework1/02-variables/src/Variables.java
@@ -0,0 +1,17 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+        int numOfStudents = 12;
+
+        String message = "Number of students: ";
+        
+        System.out.println(message + numOfStudents);
+        System.out.println(message + numOfStudents);
+        System.out.println("No of students: " + numOfStudents);
+        System.out.println("No of students: " + numOfStudents);
+        System.out.println("No of students: " + numOfStudents);
+	}
+
+}
