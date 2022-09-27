50476c84ecb7852e35dd74feefc433b7547ea340
nurseld <61293125+nurseld@users.noreply.github.com>
2022-09-27 22:59:26 +0300

    Homework1

---
+++ Homework1/03-dataTypes/src/DataTypes.java
@@ -0,0 +1,16 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+	
+		byte num1 = 12;
+		//num1 = 128; byte -------> -128 to 127 
+		
+		double number = 12.5;
+		
+		char character = 'A';
+		
+		boolean isTrue = false;
+	}
+
+}
